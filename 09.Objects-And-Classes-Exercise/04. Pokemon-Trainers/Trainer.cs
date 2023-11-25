82ce6bcba9d0b252970d8bfd8545e19ad2556f9a
Alexander Dimitrov <adimitrovx@gmail.com>
2023-11-25 23:25:36 +0200

    -

---
+++ 09.Objects-And-Classes-Exercise/04. Pokemon-Trainers/Trainer.cs
@@ -0,0 +1,14 @@
+﻿internal class Trainer
+{
+    public string TrainerName { get; set; }
+    public int NumberOfBadges { get; set; }
+    public List<Pokemon> PokemonCollection { get; set; }
+
+    public Trainer(string trainerName)
+    {
+        TrainerName = trainerName;
+        NumberOfBadges = 0;
+        PokemonCollection = new List<Pokemon>();
+    }
+}
+
