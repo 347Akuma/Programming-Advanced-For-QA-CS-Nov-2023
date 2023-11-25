b51da510c780d3633023340dd91c4658f8d75edb
Alexander Dimitrov <adimitrovx@gmail.com>
2023-11-25 19:38:44 +0200

    -

---
+++ 09.Objects-And-Classes-Exercise/01. Students/Program.cs
@@ -0,0 +1,23 @@
+﻿int studentsCount = int.Parse(Console.ReadLine());
+
+List<Student> students = new List<Student>();
+
+for (int i = 1; i <= studentsCount; i++)
+{
+    string[] input = Console.ReadLine().Split();
+
+    string firstname = input[0];
+    string secondname = input[1];
+    double grade = double.Parse(input[2]);
+
+    Student student = new Student(firstname, secondname, grade);
+
+    students.Add(student);
+}
+
+students = students.OrderByDescending(s => s.Grade).ToList(); // prezapisvame spisuka sled sortiraneto, aka davame mu finalnata stoinost
+
+foreach (Student student in students)
+{
+    Console.WriteLine($"{student.FirstName} {student.LastName}: {student.Grade:f2}");
+}
---
+++ 09.Objects-And-Classes-Exercise/01. Students/Student.cs
@@ -0,0 +1,39 @@
+﻿
+internal class Student
+{
+    // fields -> harakteristiki na klasa
+
+    private string firstName;
+    private string lastName;
+    private double grade;
+
+    //properties -> osigurqvame si dostup do poletata
+
+    public string FirstName
+    {
+        get { return firstName; } // get current value
+        set { firstName = value; } // set new value
+    }
+
+    public string LastName
+    {
+        get { return lastName; }
+        set { lastName = value; }
+    }
+
+    public double Grade
+    {
+        get { return grade; }
+        set { grade = value; }
+    }
+
+    //constructor -> method, chrez koito suzdavame obekti ot tozi klas       
+    public Student(string firstName, string lastName, double grade) // priema stoinostite
+    {
+        // suzdava se publichen prazen obekt (s imeto na klasa) sus vsichkite harakteristiki na klasa
+        FirstName = firstName; // sreshtu field FirstName -> stoinostta na firstName
+        LastName = lastName;
+        Grade = grade;
+
+    }
+}
