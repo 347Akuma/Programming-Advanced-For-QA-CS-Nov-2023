299c4d93e6b522609c05d49ed15db723d40fcd71
Alexander Dimitrov <adimitrovx@gmail.com>
2023-11-25 22:58:13 +0200

    -

---
+++ 09.Objects-And-Classes-Exercise/05. Pokemon-Trainers-Temp/Trainer.cs
@@ -0,0 +1,14 @@
+﻿public class Trainer
+{
+    public string Name { get; set; }
+    public int Badges { get; set; }
+    public List<Pokemon> PokemonCollection { get; set; }
+
+    public Trainer(string name)
+    {
+        Name = name;
+        Badges = 0;
+        PokemonCollection = new List<Pokemon>();
+    }
+}
+
