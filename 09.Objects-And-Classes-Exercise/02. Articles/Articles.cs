2e28dcbba1e9aa49e3cbcd2699e5831c886a4fd2
Alexander Dimitrov <adimitrovx@gmail.com>
2023-11-25 20:39:31 +0200

    -

---
+++ 09.Objects-And-Classes-Exercise/02. Articles/Articles.cs
@@ -0,0 +1,38 @@
+﻿internal class Article
+{
+    // harakteristiki -> auto-properties -> automatically creates a private field -> gets + sets a value
+
+    public string Title { get; set; } // creates a private string field called 'title' + get/set for access under Title property.
+    public string Content { get; set; }
+    public string Author { get; set; }
+
+
+    public Article(string title, string content, string author)
+    {
+        Title = title;
+        Content = content;
+        Author = author;
+    }
+
+    // actions that can be performed with our Article: create Edit, ChangeAuthor, Rename, Override .ToString(); methods 
+    public void Edit(string newContent)
+    {
+        Content = newContent; // prezapisvame sudurjanieto na Content s podadenoto newContent prieto ot metoda
+    }
+
+    public void ChangeAuthor (string newAuthor)
+    {
+        Author = newAuthor; // prezapisvame tekushtiq avtor na statiqta s podadeniq newAuthor kum metoda
+    }
+
+    public void Rename (string newTitle)
+    {
+        Title = newTitle;
+    }
+
+    public override string ToString() // rewrite .ToString's behavior and make it perform the below instead when called
+    {
+        return $"{Title} - {Content}: {Author}";
+    }
+}
+
---
+++ 09.Objects-And-Classes-Exercise/02. Articles/Program.cs
@@ -0,0 +1,33 @@
+﻿string[] articleData = Console.ReadLine().Split(", ");
+int numberOfCommands = int.Parse(Console.ReadLine());
+
+string title = articleData[0];
+string content = articleData[1];
+string author = articleData[2];
+
+Article article = new Article(title, content, author);
+
+for (int i = 1; i <= numberOfCommands; i++)
+{
+    string[] command = Console.ReadLine().Split(": ");
+
+    string action = command[0];
+    string parameter = command[1];
+
+    switch (action) // pri vseki sluchai si vikame metodcheto
+    {
+        case "Edit":
+            article.Edit(parameter);
+            break;
+        case "ChangeAuthor":
+            article.ChangeAuthor(parameter);
+            break;
+        case "Rename":
+            article.Rename(parameter);
+            break;
+    }
+}
+
+// printer
+
+Console.WriteLine(article.ToString());
