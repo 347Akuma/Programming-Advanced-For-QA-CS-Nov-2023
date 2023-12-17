7d4c4cecb178b5cfec26c94780809186fb86bcbd
Alexander Dimitrov <adimitrovx@gmail.com>
2023-12-17 15:54:04 +0200

    -

---
+++ 13. OOP-Principles-Encapsulation-Inheritance/BoxData/Box.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+
+namespace BoxData;
+
+public class Box
+{
+    private double _length;
+    private double _height;
+    private double _width;
+
+    public Box(double length, double height, double width)
+    {
+        this.Length = length;
+        this.Height = height;
+        this.Width = width;
+    }
+
+    public double Length
+    {
+        get { return this._length; }
+        private set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException($"Length cannot be zero or negative");
+            }
+            this._length = value;
+        }
+    }
+
+    public double Height
+    {
+        get { return this._height; }
+        private set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException($"Height cannot be zero or negative");
+            }
+            this._height = value;
+        }
+    }
+
+    public double Width
+    {
+        get { return this._width; }
+        private set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException($"Width cannot be zero or negative");
+            }
+            this._width = value;
+        }
+    }
+
+    public double SurfaceArea()
+    {
+        return 2 * this.Length * this.Width + 2 * this.Length * this.Height + 2 *
+            this.Width * this.Height;
+    }
+
+    public double Volume()
+    {
+        return this.Length * this.Height * this.Width;
+    }
+
+    public override string ToString()
+    {
+        return $"Surface Area - {this.SurfaceArea():F2}{Environment.NewLine}Volume - {this.Volume():F2}";
+    }
+}
---
+++ 13. OOP-Principles-Encapsulation-Inheritance/PersonInfo/Person.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace PersonInfo;
+
+public class Person
+{
+    private string _firstName;
+    private string _lastName;
+    private int _age;
+
+    private const int NAME_MIN_LENGTH = 3;
+
+    public Person(string firstName, string lastName, int age) //add constructor
+    {
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.Age = age;
+    }
+
+    public string FirstName // at least 3 symbols
+    {
+        get { return this._firstName; }
+        private set
+        {
+            if (value.Length < NAME_MIN_LENGTH)
+            {
+                throw new ArgumentException($"First name cannot contain fewer than {NAME_MIN_LENGTH} symbols!");
+            }
+            this._firstName = value;
+        }
+    }
+
+    public string LastName
+    {
+        get { return this._lastName; }
+        private set
+        {
+            if (value.Length < 3)
+            {
+                throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+            }
+            this._lastName = value;
+        }
+    }
+
+    public int Age // 0 or negative
+    {
+        get { return this._age; }
+        private set 
+        { 
+            if (value <= 0)
+            {
+                throw new InvalidOperationException("Age cannot be zero or a negative integer!");
+            }
+            this._age = value; 
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"{this.FirstName} {this.LastName} is {this.Age} years old.";
+    }
+}
+
+ 
